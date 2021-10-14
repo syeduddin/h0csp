7a4cd76823b28d988f47f6f664c2e085a7acb2d1
Syed A Uddin <syedauddin@gmail.com>
2021-10-14 18:03:23 -0500

    Latest Version

--- scripts/misc/PlotAll.py
+++ scripts/misc/PlotAll.py
@@ -3,17 +3,19 @@ from astropy.io import ascii
 import numpy as np
 from astropy.table import join
 import sys
-tab = ascii.read('../data/CSPI+II_max/B_max.csv')
-tab1 = ascii.read('../data/working/B_ceph.csv')
-tab2 = ascii.read('../data/working/B_trgb.csv')
-tab3 = ascii.read('../data/working/B_sbf.csv')
+tab = ascii.read('../../data/lc/B_max.csv')
+tab1 = ascii.read('../../data/working/B_ceph.csv')
+tab2 = ascii.read('../../data/working/B_trgb.csv')
+tab3 = ascii.read('../../data/working/B_sbf.csv')
 w1=np.where(tab1['dist']>1.0)
 w2=np.where(tab2['dist']>1.0)
 w3=np.where(tab3['dist']>1.0)
 w=np.where(tab1['dist']==-1.0)
 
-s1 = np.where(tab1['sample']=='CSPI')
-s2 = np.where(tab1['sample']=='CSPII')
+s = np.where(tab1['dist']==-1)
+
+s1 = np.where((tab1['sample']=='CSPI') &(tab1['dist']==-1.0) )
+s2 = np.where((tab1['sample']=='CSPII') & (tab1['dist']==-1.0))
 
 
 pl.figure(1)
@@ -25,7 +27,7 @@ pl.legend(numpoints=1,loc='upper left')
 pl.grid()
 pl.xlabel(r'$Color-stretch \ parameter \ (s_{BV})$',fontsize=14)
 pl.ylabel(r'$Color \ (B-V)$',fontsize=14)
-pl.savefig('../plots/st_bv.pdf')
+pl.savefig('../../plots/st_bv.pdf')
 
 
 pl.figure(2)
@@ -44,15 +46,17 @@ pl.hist(tab3['m'][w3],bins=10,range=[7,12],histtype='step',label='SBF',color='#3
 pl.xlabel(r'$Log \ host \ M_{stellar} \ (M_{\odot})$',fontsize=18), pl.ylabel('$Number$',fontsize=18)
 #pl.ylim(0,30),pl.xlim(7,12)
 pl.legend(loc='upper left'),pl.grid()
-pl.savefig('../plots/massdist.pdf')
+pl.savefig('../../plots/massdist.pdf')
 
 
 pl.figure(3)
 
-pl.hist(tab1['zcmb'],range=[0,.14],bins=20,histtype='stepfilled',label='CSP',color='k', alpha=.3)
+
+
+pl.hist(tab1['zcmb'][s],range=[0,.14],bins=20,histtype='stepfilled',label='CSP',color='k', alpha=.3)
 pl.hist(tab1['zcmb'][s1],range=[0,.14],bins=20,histtype='step',label='CSPI',color='r',ls='-',lw=2)
 pl.hist(tab1['zcmb'][s2],range=[0,0.14],bins=20,histtype='step',label='CSPII',color='b',ls='-',lw=2)
 pl.legend(loc='upper right'),pl.grid()
 pl.xlabel(r'$z_{CMB}$',fontsize=14), pl.ylabel('$Number$',fontsize=14)
 
-pl.savefig('../plots/zdist.pdf')
+pl.savefig('../../plots/zdist.pdf')
